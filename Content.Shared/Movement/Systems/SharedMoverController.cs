6a53f17b73390fa909d5d14c6104f7b05a07cb6e
Sprinkle <some@test.com>
2026-05-16 17:04:53 +0300

    final fixes

--- Content.Shared/Movement/Systems/SharedMoverController.cs
+++ Content.Shared/Movement/Systems/SharedMoverController.cs
@@ -118,7 +118,7 @@ using Content.Shared.Maps;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Movement.Components;
 using Content.Shared.Movement.Events;
-using Content.Shared._DV.Abilities; // DeltaV - CrawlUnderObjectsComponent
+using Content.Shared._DV.Abilities;
 using Content.Shared._DV.StepTrigger.Components; // DeltaV - NoShoesSilentFootstepsComponent
 using Content.Shared.Tag;
 using Robust.Shared.Audio;
@@ -177,7 +177,7 @@ public abstract partial class SharedMoverController : VirtualController
     protected EntityQuery<RelayInputMoverComponent> RelayQuery;
     protected EntityQuery<PullableComponent> PullableQuery;
     protected EntityQuery<TransformComponent> XformQuery;
-    protected EntityQuery<CrawlUnderObjectsComponent> CrawlUnderObjectsQuery; // DeltaV - Rodentia silent sneak footsteps
+    protected EntityQuery<CrawlUnderObjectsComponent> CrawlUnderObjectsQuery;
     protected EntityQuery<NoShoesSilentFootstepsComponent> NoShoesSilentQuery; // DeltaV - NoShoesSilentFootstepsComponent
 
     private static readonly ProtoId<TagPrototype> FootstepSoundTag = "FootstepSound";
@@ -215,7 +215,7 @@ public abstract partial class SharedMoverController : VirtualController
         MapGridQuery = GetEntityQuery<MapGridComponent>();
         FixturesQuery = GetEntityQuery<FixturesComponent>(); // Tile Movement Change
         TileMovementQuery = GetEntityQuery<TileMovementComponent>(); // Tile Movement Change
-        CrawlUnderObjectsQuery = GetEntityQuery<CrawlUnderObjectsComponent>(); // DeltaV - Rodentia silent sneak footsteps
+        CrawlUnderObjectsQuery = GetEntityQuery<CrawlUnderObjectsComponent>();
         NoShoesSilentQuery = GetEntityQuery<NoShoesSilentFootstepsComponent>(); // DeltaV - NoShoesSilentFootstepsComponent
         MapQuery = GetEntityQuery<MapComponent>();
 
@@ -688,10 +688,8 @@ public abstract partial class SharedMoverController : VirtualController
 
         mobMover.StepSoundDistance -= distanceNeeded;
 
-        // DeltaV - Rodentia are silent while their species sneak mode is enabled.
         if (CrawlUnderObjectsQuery.TryComp(uid, out var crawlUnderObjects) && crawlUnderObjects.Enabled)
             return false;
-        // End DeltaV code
 
         // DeltaV - Don't play the sound if they have no shoes and the component
         if (NoShoesSilentQuery.HasComp(uid) &&
